898ffaa67820202bb3e6c820dc824f4fd2bd12c6
Fabyo Ramos <42546627+fabyosr@users.noreply.github.com>
2025-05-26 21:24:34 -0300

    Update call agent function

--- gemini_ai_agent_antifraude.py
+++ gemini_ai_agent_antifraude.py
@@ -20,7 +20,30 @@ MODEL_ID = "gemini-2.0-flash"
 session_service = InMemorySessionService()
 
 # Função auxiliar que envia uma mensagem para um agente via Runner e retorna a resposta final
+
 async def call_agent(agent: Agent, message_text: str) -> str:
+
+    # Cria uma nova sessão (você pode personalizar os IDs conforme necessário)
+    session = await session_service.create_session(app_name=agent.name, user_id="user1")
+
+    # Cria um Runner para o agente
+    runner = Runner(agent=agent, app_name=agent.name, session_service=session_service)
+
+    # Cria o conteúdo da mensagem de entrada
+    content = types.Content(role="user", parts=[types.Part(text=message_text)])
+
+    final_response = ""
+    # Itera assincronamente pelos eventos retornados durante a execução do agente
+    async for event in runner.run_async(user_id="user1", session_id=session.id, new_message=content):
+        if event.is_final_response():
+          for part in event.content.parts:
+            if part.text is not None:
+              final_response += part.text
+              final_response += "\n"
+    return final_response        
+
+# Função auxiliar que envia uma mensagem para um agente via Runner e retorna a resposta final
+async def call_agent_old(agent: Agent, message_text: str) -> str:
    
     # Cria uma nova sessão (você pode personalizar os IDs conforme necessário)
     session = await session_service.create_session(app_name=agent.name, user_id="user1", session_id="session1")
